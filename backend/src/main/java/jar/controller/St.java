9dd513c5aa1cb09bb410151ad65e4eba7ffa3c37
Badrinath-ml <badrinathreddy2416@gmail.com>
2026-08-02 11:48:27 +0530

    add update and delete endpoints for Student API

--- backend/src/main/java/jar/controller/St.java
+++ backend/src/main/java/jar/controller/St.java
@@ -5,8 +5,11 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +53,44 @@ public class St {
 
         db.save(s);
 
+        return res;
+    }
+     @PutMapping("/update/{id}")
+    Map<Object, Object> m4(@RequestBody Student d, @PathVariable("id") long id) {
+        Map<Object, Object> res = new HashMap<>();
+        res.put("msg", "welcome to Put api");
+        res.put("status", 200);
+        Student s = db.findById(id).orElse(null);
+        if (s != null) {
+            if (d.getName() != null) {
+                s.setName(d.getName());
+            }
+            if (d.getEmail() != null) {
+                s.setEmail(d.getEmail());
+            }
+            if (d.getIp() != null) {
+                s.setIp(d.getIp());
+            }
+            db.save(s);
+        } else {
+            res.put("msg", "Student not found");
+            res.put("status", 404);
+        }
+        return res;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    Map<Object, Object> m5(@PathVariable("id") long id) {
+        Map<Object, Object> res = new HashMap<>();
+        res.put("msg", "welcome to Delete api");
+        res.put("status", 200);
+        Student s = db.findById(id).orElse(null);
+        if (s != null) {
+            db.delete(s);
+        } else {
+            res.put("msg", "Student not found");
+            res.put("status", 404);
+        }
         return res;
     }
 
