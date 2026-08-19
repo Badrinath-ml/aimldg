86253f6f4b897272e1a9c781b8f57f61166c531b
Badrinath <badrinathreddy2416@gmail.com>
2026-08-19 14:01:48 +0530

    Added Services

--- backend/src/main/java/jar/service/Dservice.java
+++ backend/src/main/java/jar/service/Dservice.java
@@ -34,4 +34,57 @@ public class Dservice {
         return res;
     }
 
+     public Map<Object, Object> create(Student d) {
+        
+        Map<Object, Object> res = new HashMap<>();
+
+        Student s = new Student();
+        s.setName(d.getName());
+        s.setEmail(d.getEmail());
+        s.setIp(d.getIp());
+
+        db.save(s);
+
+        res.put("msg", "Student Added Successfully");
+        res.put("status", 201);
+        res.put("data", s);
+
+        return res;
+    }
+
+    public Map<Object, Object> update(Integer id, Student d) {
+        Map<Object, Object> res = new HashMap<>();
+
+        if (db.existsById(id)) {
+
+            Student s = db.findById(id).get();
+            s.setName(d.getName());
+            s.setEmail(d.getEmail());
+            s.setIp(d.getIp());
+
+            db.save(s);
+
+            res.put("msg", "Student Updated Successfully");
+            res.put("status", 200);
+            res.put("data", s);
+
+        } else {
+
+            res.put("msg", "Student Not Found");
+            res.put("status", 404);
+
+        }
+        return res;
+    }
+
+    public Map<Object, Object> read() {
+        Map<Object, Object> res = new HashMap<>();
+
+        res.put("msg", "Student Fetched Successfully");
+        res.put("status", 200);
+        res.put("data", db.findAll());
+
+        return res;
+    }
+
 }
